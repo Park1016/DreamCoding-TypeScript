65257c6ccc813f10cd645eb6c796bb76051416cd
hyunjeongPark <GuoHJP@gmail.com>
2021-03-13 02:56:16 +0900

    스스로 짠 코드(계산기 함수 만들기)

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,6 +1,20 @@
 /**
  * Let's make a calculator 🧮
  */
+function calculate(str:string, a:number, b:number):number {
+    if(str === 'add'){
+        return a + b;
+    }else if(str === 'substract'){
+        return a - b;
+    }else if(str === 'multiply'){
+        return a * b;
+    }else if(str === 'divide'){
+        return a / b;
+    }else if(str === 'remainder'){
+        return a % b;
+    }
+}
+
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
