1e188513ac32160187b137f2252e4ebdce6f9b7e
hyunjeongPark <GuoHJP@gmail.com>
2021-03-13 02:27:33 +0900

    타입 추론 강의 정리

---
+++ 1-types/1-9-inference.ts
@@ -0,0 +1,16 @@
+﻿/**
+ * Type Inference
+ * 타입 추론
+ * 타입을 명확하게 명시하지 않아도 알아서 타입이 결정되는 경우
+ */
+ let text = 'hello';
+ function print(message = 'hello') {
+   console.log(message);
+ }
+ print('hello');
+ 
+ function add(x: number, y: number): number {
+   return x + y;
+ }
+ const result = add(1, 2);
+ 
