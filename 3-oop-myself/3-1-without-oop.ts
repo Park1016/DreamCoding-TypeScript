77ccba267386757b724c262191065c38212f4d5c
hyunjeongPark <GuoHJP@gmail.com>
2021-03-16 04:20:41 +0900

    절차지향적으로 커피기계 만들기(스스로 짠 코드)

---
+++ 3-oop-myself/3-1-without-oop.ts
@@ -0,0 +1,20 @@
+﻿const beans:number = 1;
+
+function makeCoffee(shot:number):string{
+    const coffee = beans * shot;
+    switch(coffee){
+        case 1:
+            return `1 shot coffee`;
+        case 2:
+            return `2 shot coffee`;
+        case 3:
+            return `3 shot coffee`;
+        default:
+            throw new Error(`unknown shot`);
+    }
+}
+
+console.log(makeCoffee(1));
+console.log(makeCoffee(2));
+console.log(makeCoffee(3));
+console.log(makeCoffee(4));
