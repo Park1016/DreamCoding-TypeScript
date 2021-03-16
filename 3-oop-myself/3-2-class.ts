749d9283efa62eeb6a8b41e8bdbe044d4804a10a
hyunjeongPark <GuoHJP@gmail.com>
2021-03-16 19:25:51 +0900

    객체지향적으로 커피기계 만들기-스스로 하기 실패, 강의 들은 후 강의코드를 외워서 친 코드

---
+++ 3-oop-myself/3-2-class.ts
@@ -0,0 +1,33 @@
+﻿{
+    type CoffeeCup = {
+      shots: number;
+      hasMilk: boolean;
+    };
+  
+    class CoffeeMaker {
+        static BEANS_GRAMM_PER_SHOT: number = 7;
+        coffeeBeans: number = 0;
+
+        constructor(beans:number){
+            this.coffeeBeans = beans;
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+          if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+            throw new Error('Not enough coffee beans!');
+          }
+          this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+          return {
+            shots,
+            hasMilk: false,
+          };
+        }
+    }
+
+    const coffee = new CoffeeMaker(2);
+    const coffee2 = new CoffeeMaker(3);
+
+    console.log(coffee);
+    console.log(coffee2);
+}
+  
